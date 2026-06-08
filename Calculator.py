403ba403a3a199abcd92c77139e253103d4e7c00
Harman Sihag <harman.sihag.edu@gmail.com>
2026-06-08 19:18:35 +0530

    Add files via upload
    Calculator using python

---
+++ Calculator.py
@@ -0,0 +1,28 @@
+#Calculator with +-%/*//** operations
+num1= float(input("First number :"))
+num2 = float(input("Second number :"))
+operator = str(input("Enter the operation:"))
+
+if operator != "+" and operator != "-" and operator != "*" and operator != "**" and operator != "/" and operator != "//" and operator != "%" : 
+    print("INVALID OPERATOR") 
+elif operator == "+" : 
+    result = num1 + num2
+    print(round(result, 4))
+elif operator == "-" : 
+    result = num1 - num2
+    print(round(result, 4))
+elif operator == "*" : 
+    result = num1*num2
+    print(round(result, 4))
+elif operator == "**" :  # num1 to the power num2
+    result = num1**num2
+    print(round(result, 4))
+elif operator == "/" : 
+    result = num1 / num2
+    print(round(result, 4))
+elif operator == "//" : # Round off the quotient
+    result = num1 // num2
+    print(round(result, 4))
+elif operator == "%" : # gives the remainder 
+    result = num1 % num2
+    print(round(result, 4))
